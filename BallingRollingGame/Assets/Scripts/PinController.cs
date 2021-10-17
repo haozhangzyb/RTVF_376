ab98fe83b81e38f09c22736fc20040bc6e0122a0
HaoOneBin <37146238+HaoOneBin@users.noreply.github.com>
2021-10-16 21:13:49 -0500

    Recomit All Change

---
+++ BallingRollingGame/Assets/Scripts/PinController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinController : MonoBehaviour
+{
+    private bool isHit;
+    public PlayerController playerScore;
+
+ 
+    void Start()
+    {
+      
+        isHit = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Floor") && !isHit)
+        {
+            isHit = true;
+            Debug.Log("Pin has fallen");
+            playerScore.score += 5;
+            playerScore.SetScoreText();
+        }
+    }
+}
